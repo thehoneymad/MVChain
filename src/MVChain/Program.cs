487bd35cc4b719496e18a05d31864ff6fd6c73ff
Swagata Prateek <thehoneymad@Swagatas-Mac-mini.local>
2018-01-16 23:14:00 -0800

    Stage 1: Hello World

---
+++ src/MVChain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
